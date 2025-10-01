44f13f074ef7f6f98fa62db10cceab68fc5bdee5
Jaime <jaimevazquezmartin@protonmail.com>
2025-10-01 16:30:22 +0200

    Add test for checking when message displayed when no match saved

---
+++ match/presentation/src/androidTest/java/com/vzkz/match/presentation/match_history/MatchHistoryScreenRobot.kt
@@ -0,0 +1,24 @@
+package com.vzkz.match.presentation.match_history
+
+import android.content.Context
+import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.junit4.ComposeContentTestRule
+import androidx.compose.ui.test.onNodeWithContentDescription
+import androidx.compose.ui.test.onNodeWithText
+import com.vzkz.match.presentation.R
+import kotlinx.coroutines.runBlocking
+
+class MatchHistoryScreenRobot(
+    private val composeActivityRule: ComposeContentTestRule,
+    private val context: Context
+) {
+    fun add_first_match_text_displayed(): MatchHistoryScreenRobot {
+        runBlocking {
+            composeActivityRule
+                .onNodeWithText(context.getString(R.string.create_you_first_match))
+                .assertIsDisplayed()
+        }
+
+        return this
+    }
+}
