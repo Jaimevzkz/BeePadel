8049789072a74eb77a501946d50d76fe6f4edb4e
Jaime <jaimevazquezmartin@protonmail.com>
2025-10-01 17:37:14 +0200

    move whatsnew directory

---
+++ .github/createReleaseBranchAndTriggerWorkflow.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+if [ "$#" -lt 2 ] || [ "$#" -gt 3 ]; then
+  echo "Usage: $0 <*.*.*> <mobile|wear> [mobile|wear]"
+  exit 1
+fi
+
+if [[ ! "$1" =~ ^[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
+    echo "Invalid version name, it should follow the *.*.* regex"
+    exit 1
+fi
+
+branchName=$1/release
+
+git checkout master
+git checkout -b $branchName
+git push origin $branchName
+./triggerWorkflow.sh $2 $3
+
+git checkout master
