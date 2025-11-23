f1a5f00fee5aaedd6f7936d2250c290aa1b82d8c
Jaime <jaimevazquezmartin@protonmail.com>
2025-11-23 13:04:11 +0100

    Organize settings screen and start working on about section
    Organize settings screen and start working on about section

---
+++ settings/presentation/src/main/java/com/vzkz/beepadel/settings/presentation/general_settings/components/BooleanSetting.kt
@@ -0,0 +1,40 @@
+package com.vzkz.beepadel.settings.presentation.general_settings.components
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Switch
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun BooleanSetting(
+    modifier: Modifier = Modifier,
+    title: String,
+    value: Boolean,
+    onValueChange: () -> Unit
+) {
+    Row(
+        modifier = modifier
+            .fillMaxWidth()
+            .clip(RoundedCornerShape(8.dp))
+            .background(MaterialTheme.colorScheme.surfaceContainer)
+            .padding(12.dp),
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Text(text = title, color = MaterialTheme.colorScheme.onSurface)
+        Switch(
+            checked = value,
+            onCheckedChange = { onValueChange() },
+        )
+    }
+}
---
+++ settings/presentation/src/main/java/com/vzkz/beepadel/settings/presentation/general_settings/components/SectionTitle.kt
@@ -0,0 +1,21 @@
+package com.vzkz.beepadel.settings.presentation.general_settings.components
+
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun SectionTitle(
+    modifier: Modifier = Modifier,
+    text: String
+) {
+    Text(
+        modifier = modifier.padding(vertical = 8.dp),
+        text = text,
+        style = MaterialTheme.typography.headlineSmall
+    )
+}
