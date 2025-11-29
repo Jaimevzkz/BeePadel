bdad7d8c7fac843f603bf3a9399abb3183e444e1
Jaime <jaimevazquezmartin@protonmail.com>
2025-11-29 19:57:58 +0100

    Add logged with beepadel strava setting

--- common/general/src/main/java/com/vzkz/common/general/SettingKeys.kt
+++ common/general/src/main/java/com/vzkz/common/general/SettingKeys.kt
@@ -5,3 +5,9 @@ object GOLDEN_POINT {
     const val KEY = "golden_point"
     const val DEFAULT_VAL = false
 }
+
+@Suppress("ClassName")
+object LOGGED_WITH_BEEPADEL {
+    const val KEY = "logged_with_beepadel"
+    const val DEFAULT_VAL = false
+}
--- settings/presentation/src/main/java/com/vzkz/beepadel/settings/presentation/strava_settings/StravaSettingsContract.kt
+++ settings/presentation/src/main/java/com/vzkz/beepadel/settings/presentation/strava_settings/StravaSettingsContract.kt
@@ -1,5 +1,6 @@
 package com.vzkz.beepadel.settings.presentation.strava_settings
 
+import com.vzkz.common.general.LOGGED_WITH_BEEPADEL
 import com.vzkz.core.presentation.ui.UiText
 import com.vzkz.core.presentation.ui.model.Event
 import com.vzkz.core.presentation.ui.model.Intent
@@ -7,10 +8,12 @@ import com.vzkz.core.presentation.ui.model.State
 
 data class StravaSettingsState(
     val error: UiText?,
+    val loggedWithBeePadelEnabled: Boolean
 ) : State {
     companion object {
         val initial: StravaSettingsState = StravaSettingsState(
             error = null,
+            loggedWithBeePadelEnabled = LOGGED_WITH_BEEPADEL.DEFAULT_VAL,
         )
     }
 }
@@ -18,6 +21,7 @@ data class StravaSettingsState(
 sealed class StravaSettingsIntent : Intent {
     data object LogoutFromStrava: StravaSettingsIntent()
     data object NavigateBack: StravaSettingsIntent()
+    data object ToggleLoggedWithBeePadel: StravaSettingsIntent()
 }
 
 sealed class StravaSettingsEvent : Event {
