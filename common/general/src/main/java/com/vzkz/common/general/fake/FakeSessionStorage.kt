656383e1b76b12dcc760a43f7cb5fe3cc334ab5d
Jaime <jaimevazquezmartin@protonmail.com>
2025-11-29 19:16:04 +0100

    Fix fakes

--- common/general/src/main/java/com/vzkz/common/general/fake/FakeSessionStorage.kt
+++ common/general/src/main/java/com/vzkz/common/general/fake/FakeSessionStorage.kt
@@ -4,18 +4,20 @@ import com.vzkz.core.domain.SessionStorage
 import com.vzkz.core.domain.auth.AuthInfo
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.flowOf
 
 class FakeSessionStorage : SessionStorage {
+    private val tokensAvailableFlow = MutableStateFlow(false)
+    private var authInfo: AuthInfo? = null
+
     override val tokensAvailable: StateFlow<Boolean>
-        get() =
-            MutableStateFlow<Boolean>(false)
+        get() = tokensAvailableFlow
+
 
     override suspend fun get(): AuthInfo? {
-        return null
+        return authInfo
     }
 
     override suspend fun set(info: AuthInfo?) {
-
+        authInfo = info
     }
 }
--- common/general/src/main/java/com/vzkz/common/general/fake/FakeWatchConnector.kt
+++ common/general/src/main/java/com/vzkz/common/general/fake/FakeWatchConnector.kt
@@ -2,24 +2,35 @@ package com.vzkz.common.general.fake
 
 import com.vzkz.core.connectivity.domain.messaging.MessagingAction
 import com.vzkz.core.connectivity.domain.model.DeviceNode
+import com.vzkz.core.domain.error.DataError
 import com.vzkz.core.domain.error.EmptyResult
 import com.vzkz.core.domain.error.MessagingError
 import com.vzkz.core.domain.error.Result
 import com.vzkz.match.domain.WatchConnector
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asSharedFlow
 import kotlinx.coroutines.flow.emptyFlow
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.flowOf
 
 class FakeWatchConnector: WatchConnector {
+
+    var errorToReturn: MessagingError? = null
+
     override val connectedDevice: StateFlow<DeviceNode?>
         get() = MutableStateFlow<DeviceNode?>(null)
+
+    private var _messagingActions = MutableSharedFlow<MessagingAction>()
     override val messagingActions: Flow<MessagingAction>
-        get() = emptyFlow<MessagingAction>()
+        get() = _messagingActions.asSharedFlow()
 
     override suspend fun sendActionToWatch(action: MessagingAction): EmptyResult<MessagingError> {
+        if (errorToReturn != null) return Result.Error(errorToReturn!!)
+
+        _messagingActions.emit(action)
         return Result.Success(Unit)
     }
 }
