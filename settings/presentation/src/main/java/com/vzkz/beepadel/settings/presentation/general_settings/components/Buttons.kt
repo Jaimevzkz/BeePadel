f173f0dde1cec2df287bc86efb7498dc34ae3604
Jaime <jaimevazquezmartin@protonmail.com>
2025-12-21 13:40:37 +0100

    Reorganize settings screen

--- settings/presentation/src/main/java/com/vzkz/beepadel/settings/presentation/general_settings/components/Buttons.kt
+++ settings/presentation/src/main/java/com/vzkz/beepadel/settings/presentation/general_settings/components/Buttons.kt
@@ -1,15 +1,37 @@
 package com.vzkz.beepadel.settings.presentation.general_settings.components
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Check
+import androidx.compose.material.icons.filled.Download
+import androidx.compose.material.icons.filled.ImportExport
 import androidx.compose.material.icons.filled.Info
 import androidx.compose.material.icons.filled.Mail
+import androidx.compose.material.icons.filled.MoreVert
+import androidx.compose.material.icons.filled.Upload
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
@@ -17,6 +39,7 @@ import com.vzkz.common.general.R
 import com.vzkz.core.presentation.designsystem.GithubIcon
 import com.vzkz.core.presentation.designsystem.PlayStoreIcon
 import com.vzkz.core.presentation.designsystem.StravaIcon
+
 
 @Composable
 fun ConnectToStravaButton(
@@ -150,4 +173,76 @@ fun AboutButton(
         title = text,
         onClick = onClick
     )
+}
+
+@Composable
+fun ImportExportMatchesButton(
+    modifier: Modifier = Modifier,
+    onImport: () -> Unit,
+    onExport: () -> Unit,
+) {
+    var expanded by remember { mutableStateOf(false) }
+    Row(
+        modifier = modifier
+            .fillMaxWidth()
+            .height(80.dp)
+            .clip(RoundedCornerShape(8.dp))
+            .background(MaterialTheme.colorScheme.surfaceContainer)
+            .padding(horizontal = 12.dp, vertical = 24.dp),
+        horizontalArrangement = Arrangement.Start,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Icon(
+            modifier = Modifier
+                .padding(end = 8.dp)
+                .size(20.dp),
+            imageVector = Icons.Default.ImportExport,
+            contentDescription = stringResource(R.string.import_export_matches)
+        )
+        Text(
+            text = stringResource(R.string.import_export_matches),
+            color = MaterialTheme.colorScheme.onSurface
+        )
+        Spacer(Modifier.weight(1f))
+        Box() {
+            IconButton(onClick = { expanded = true }) {
+                Icon(
+                    imageVector = Icons.Default.MoreVert,
+                    contentDescription = "Open dropdown"
+                )
+            }
+            DropdownMenu(
+                expanded = expanded,
+                onDismissRequest = { expanded = false }
+            ) {
+                DropdownMenuItem(
+                    leadingIcon = {
+                        Icon(
+                            imageVector = Icons.Default.Upload,
+                            contentDescription = stringResource(R.string.import_matches)
+                        )
+                    },
+                    text = { Text(stringResource(R.string.import_matches)) },
+                    onClick = {
+                        expanded = false
+                        onImport()
+                    }
+                )
+                DropdownMenuItem(
+                    leadingIcon = {
+                        Icon(
+                            imageVector = Icons.Default.Download,
+                            contentDescription = stringResource(R.string.export_matches)
+                        )
+                    },
+                    text = { Text(stringResource(R.string.export_matches)) },
+                    onClick = {
+                        expanded = false
+                        onExport()
+                    }
+                )
+            }
+        }
+
+    }
 }
