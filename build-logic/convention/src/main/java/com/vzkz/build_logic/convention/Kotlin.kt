051f52a8a3f32c7e26e456db2bc515b8882e6f0f
Jaime <jaimevazquezmartin@protonmail.com>
2026-02-15 11:29:04 +0100

    Bump java version

--- build-logic/convention/src/main/java/com/vzkz/build_logic/convention/Kotlin.kt
+++ build-logic/convention/src/main/java/com/vzkz/build_logic/convention/Kotlin.kt
@@ -21,8 +21,8 @@ internal fun Project.configureKotlinAndroid(
 
         compileOptions {
             isCoreLibraryDesugaringEnabled = true
-            sourceCompatibility = JavaVersion.VERSION_11
-            targetCompatibility = JavaVersion.VERSION_11
+            sourceCompatibility = JavaVersion.VERSION_17
+            targetCompatibility = JavaVersion.VERSION_17
         }
     }
 
@@ -39,8 +39,8 @@ internal fun Project.configureKotlinAndroid(
 
 internal fun Project.configureKotlinJvm(){
     extensions.configure<JavaPluginExtension>{
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     configureKotlin()
 }
@@ -48,7 +48,7 @@ internal fun Project.configureKotlinJvm(){
 private fun Project.configureKotlin(){
     tasks.withType<KotlinCompile>().configureEach{
         compilerOptions{
-            jvmTarget.set(JvmTarget.JVM_11)
+            jvmTarget.set(JvmTarget.JVM_17)
         }
     }
 }
